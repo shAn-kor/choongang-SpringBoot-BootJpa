15caea1f461887e1876902c2da96795b04e479eb
shAn-kor <kjn0406@naver.com>
2024-08-07 08:53:49 +0900

    Auto push 2024-08-07  8:53:49.01

---
+++ BootBasic/src/main/java/com/simple/basic/command/TestVO.java
@@ -0,0 +1,13 @@
+package com.simple.basic.command;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class TestVO {
+    private String name;
+    private int age;
+}
---
+++ BootBasic/src/main/java/com/simple/basic/config/WebConfig.java
@@ -0,0 +1,45 @@
+package com.simple.basic.config;
+
+import com.simple.basic.command.TestVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//@PropertySource("classpath:/hello.properties") // 특정 properties 파일을 참조받기
+public class WebConfig implements WebMvcConfigurer {
+    /*@Value("${server.port}") // properties 에 있는 키를 찾아 값 받아옴
+    String port;
+
+    @Value("${hello}")
+    String hello;
+
+    @Value("${bye}")
+    String bye;
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    // 자바코드로 빈생성
+    @Bean
+    public TestVO testVO() {
+        return new TestVO(); // bean으로 등록
+    }
+
+    @Bean // 스프링이 이 코드를 실행시켜, 리턴에 담기는 값을 bean으로 등록
+    public void test() {
+//        System.out.println("스프링 설정파일 실행됨");
+//        System.out.println(applicationContext.getBean("homeController"));
+//        System.out.println(applicationContext.getBeanDefinitionCount());
+
+        System.out.println(applicationContext.getBean(TestVO.class));
+
+        System.out.println(port);
+        System.out.println(hello);
+        System.out.println(bye);
+    }*/
+}
